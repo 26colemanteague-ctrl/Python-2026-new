e1fa88840c155f91c47a985f6f0cbea084857a6c
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-02-09 16:36:25 +0000

    room divelipment

--- initial-code.py
+++ initial-code.py
@@ -1,13 +1,15 @@
+# os = operating system.
 import os, time
 # make the text type out and be seen being type insted of it just apering
 def slowText(text, delay=0.1):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
-    print()  
+    print() 
 
+# the starting of the code that is the game
 def start():
-    global playerName
+    global playerName #establishes your name as a global variable
     os.system('cls' if os.name == 'nt' else 'clear')
     slowText("Welcome to the game! Please enter your name: ")
     playerName = input()
@@ -15,7 +17,8 @@ def start():
     slowText("From here, you can go to the kitchen, the bedroom, or the garden.")
     # Further game logic would go here
     livingRoom()
-# this gives you the chose of where you want to go
+
+# this gives you the chose of where you want to go/contution of start
 def livingRoom():
     os.system('cls' if os.name == 'nt' else 'clear')
     slowText("You are in the living room. There are doors to the kitchen, bedroom, and garden.")
@@ -30,6 +33,30 @@ def livingRoom():
     else:
         print("Invalid choice. Please try again.")
         livingRoom()
+
+def kitchen ():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    slowText("You are in the kitchen. There is a doors to the living room.")
+    slowText("What would you like to do?")
+    choice = input().strip().lower()
+    if choice == "living room":
+        livingRoom()
+    else:
+        print("Invalid choice. Please try again.")
+        kitchen()
+
+def bedroom():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    slowText("You are in the bed room. there is a door to the living room.")
+    slowText("What would you like to do?")
+    choice = input().strip().lower()
+    if choice == "living room":
+        livingRoom()
+    else:
+        print("Invalid choice. Please try again.")
+        bedroom()
+
+
 # alwose you to inter your name and start the program
 playerName = ""
 start()
