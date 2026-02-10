41447355875e2055a5611551aa50562045d53e44
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-02-10 14:52:53 +0000

    all rooms done

--- initial-code.py
+++ initial-code.py
@@ -57,6 +57,20 @@ def bedroom():
         bedroom()
 
 
+def garden():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    slowText("You are in the garden. There is a door to the living room.")
+    slowText("What would you like to do?")
+    choice = input().strip().lower()
+    if choice == "living room":
+        livingRoom()
+    else:
+        print("Invalid choice. Please try again.")
+        garden()
+
+
+
+
 # alwose you to inter your name and start the program
 playerName = ""
 start()
