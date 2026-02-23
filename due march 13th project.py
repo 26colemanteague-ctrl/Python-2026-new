1521cd7944a64cc838067bdc98f38a446cd88f1d
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-02-23 17:40:46 +0000

    hope it works

---
+++ due march 13th project.py
@@ -0,0 +1,17 @@
+print(" hello welcom to the game. what is you name?")
+global playerName
+playerName= input()
+print (playerName + "you have woke up in a stange cave only lighten by a torch on the ground ")
+
+
+
+
+
+
+
+
+
+
+
+
+playerName = ""
