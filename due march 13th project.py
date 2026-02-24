774cafa53a4bc843482aa5a3b67267bcca64bb2e
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-02-24 19:25:24 +0000

    need work

--- due march 13th project.py
+++ due march 13th project.py
@@ -1,7 +1,17 @@
 print(" hello welcom to the game. what is you name?")
 global playerName
 playerName= input()
-print (playerName + "you have woke up in a stange cave only lighten by a torch on the ground ")
+import os, time, random 
+options =("left", "middel", "right", "up", "down")
+print(random.choice(options))
+os.system ('cls' if os.name == 'nt' else 'clear')
+print ("you have woke up in a stange cave only lighten by a torch on the ground. you pick up the torch and see a path to", random.choice(options))
+choice = input().strip().lower()
+if choice == "left":
+    #left()
+    print("you have desided to go left and see ")
+
+
 
 
 
