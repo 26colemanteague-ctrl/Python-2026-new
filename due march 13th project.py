d4b7490e7de9a5467e0e97e4a056988ff7b5b23b
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-02-26 14:19:34 +0000

    it not working

--- due march 13th project.py
+++ due march 13th project.py
@@ -9,9 +9,22 @@ print ("you have woke up in a stange cave only lighten by a torch on the ground.
 choice = input().strip().lower()
 if choice == "left":
     #left()
-    print("you have desided to go left and see ")
-elif choice == "middel"
-    
+    print("you have desided to go left.")
+elif choice == "middel":
+    #middle()
+    print("you have desided to go on the middel.")
+elif choice == "right":
+    #right()
+    print("you have desided to go right.")
+elif choice == "up":
+    #up()
+    print("you desided to clime up the latter.")
+elif choice == "down":
+    #down
+    print("you crowl down a tight whole leading to an other room.")
+else choice == "stay": 
+    #stay
+    print("you can't desided maybey take another look.")
 
 
 
