896022b450018f08380b713a51ed08365aa3d55c
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-03-02 19:27:13 +0000

    stuff it geting defined

--- due march 13th project.py
+++ due march 13th project.py
@@ -2,6 +2,9 @@ print(" hello welcom to the game. what is you name?")
 global playerName
 playerName= input()
 import os, time, random 
+choice = input().strip().lower()
+
+options =("left", "middel", "right", "up", "down")
 
 def down():
     os.system ('cls' if os.name == 'nt' else 'clear')
@@ -10,33 +13,49 @@ def right():
     os.system ('cls' if os.name == 'nt' else 'clear')
 
 
+def left():
+    os.system ('cls' if os.name == 'nt' else 'clear')
+if choice == "left":
+    print(random.choice(options))
 
-options =("left", "middel", "right", "up", "down")
+
+def up():
+    os.system ('cls' if os.name == 'nt' else 'clear')
+
+def middel():
+    os.system ('cls' if os.name == 'nt' else 'clear')
+
+def newRoom():
+    os.system ('cls' if os.name == 'nt' else 'clear')
+    print(random.choice(options))
+
+
 print(random.choice(options))
 os.system ('cls' if os.name == 'nt' else 'clear')
 print ("you have woke up in a stange cave only lighten by a torch on the ground. you pick up the torch and see a path to", random.choice(options))
 choice = input().strip().lower()
 if choice == "left":
-    #left()
+    left()
     print("you have desided to go left.")
 elif choice == "middel":
-    #middle()
+    middel()
     print("you have desided to go on the middel.")
 elif choice == "right":
     right()
     print("you have desided to go right.")
     print("you find your self seeing a way", random.choice(options) ("what will you do?"))
 elif choice == "up":
-    #up()
+    up()
     print("you desided to clime up the latter.")
+
 elif choice == "down":
     down()
     print("you crowl down a tight whole leading to an other room.")
     print("you go down the whole and find", random.choice(options))
-    #newRoom()
+    newRoom()
 
 else: 
-    print("you can't desided maybey take another look.")
+    print("you can't desided maybe take another look.")
 
 
 
